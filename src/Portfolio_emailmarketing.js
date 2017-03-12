2e8517036b267d830accabf92d1068c8ec36c0dc
Sarah Basinger <sarrey@gmail.com>
2017-03-11 20:05:46 -0500

    Email marketing portfolio page - move images from imgur to aws.

--- src/Portfolio_emailmarketing.js
+++ src/Portfolio_emailmarketing.js
@@ -24,7 +24,7 @@ class Portfolio_emailmarketing extends Component {
 											<BtnExternal linkTo={"http://view.aadvantageeshopping.com/?j=fe5713757162057a771c&m=fe641570776002757512&ls=fdf7177073600d7c7614767d&l=fe5a1775766c0c7f7017&s=fe3013737663067f761070&jb=ff5e12727c&ju=fe221376756502787c1275&r=0"} buttonText={"View example"} />
 									</div>
 									<div className="col-md-5 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-12">
-										<a href="http://view.aadvantageeshopping.com/?j=fe5713757162057a771c&m=fe641570776002757512&ls=fdf7177073600d7c7614767d&l=fe5a1775766c0c7f7017&s=fe3013737663067f761070&jb=ff5e12727c&ju=fe221376756502787c1275&r=0" target="_blank"><img src="http://i.imgur.com/12HPwNW.png" role="presentation" /></a>
+										<a href="http://view.aadvantageeshopping.com/?j=fe5713757162057a771c&m=fe641570776002757512&ls=fdf7177073600d7c7614767d&l=fe5a1775766c0c7f7017&s=fe3013737663067f761070&jb=ff5e12727c&ju=fe221376756502787c1275&r=0" target="_blank"><img src="/images/mktgemail_screenshot.png" role="presentation" /></a>
 									</div>
 								</div>
 							<div className="row">
@@ -36,10 +36,10 @@ class Portfolio_emailmarketing extends Component {
 										<li>Manage data with back-end team</li>
 										<li>Redesign and code corporate Cartera confirmation email</li>
 									</ul>
-										<BtnExternal linkTo={"http://i.imgur.com/coNfXdm.png"} buttonText={"View redesign"} /><br />
+										<BtnExternal linkTo={"/images/confirmation_update.png"} buttonText={"View redesign"} /><br />
 								</div>
 								<div className="col-md-4 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-12"><br />
-									<a href="http://i.imgur.com/coNfXdm.png" target="_blank"><img src="http://i.imgur.com/coNfXdm.png" role="presentation" /></a>
+									<a href="/images/confirmation_update.png" target="_blank"><img src="/images/confirmation_update.png" role="presentation" /></a>
 								</div>
 							</div>
 

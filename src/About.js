fa63abb0d886261657870b54d13d35b1a1e38a22
Sarah Basinger <sarrey@gmail.com>
2018-12-24 09:22:34 -0700

    Update about me

--- src/About.js
+++ src/About.js
@@ -17,7 +17,7 @@ class About extends Component {
 							<div>
 								<br /><p>[&nbsp;Email marketing professional turned full-stack web developer&nbsp;]</p>
 								<p>On March 24, 2017, I completed a 4-month full-stack web development training program at <a href="http://www.digitalcrafts.com" target="_blank"><strong>DigitalCrafts</strong></a> in Atlanta, GA. Before then, I spent 7 years in the email marketing industry.</p>
-								<p>Today, I am a full-stack Software Engineer at The Home Depot. I pair program and practice TDD with a team of engineers on a cloud-based Ruby on Rails / React application used by Home Depot Associates in 1,100+ retail locations across the country.</p>
+								<p>My first role after the coding bootcamp was at The Home Depot as a full-stack software engineer, working with Ruby on Rails and React. Today I am with Nutrien in Loveland, CO as a front-end developer working with React/Redux.</p> 
 								<p>When I{"'"}m not coding, you can find me reading fantasy novels, going on hikes with my dogs, traveling, playing video games, baking, taking pictures, and obsessing over anything Harry Potter or Lord of the Rings.</p>
 							</div>
 							<div className="col-xs-12 text-center">
